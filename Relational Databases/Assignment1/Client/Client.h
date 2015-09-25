a998ed450e3fdc3ca11db50a05d598b6f58bfa20
Steven Johnston <wheatthins@hotmail.ca>
2015-09-25 14:10:19 -0400

    Added files

---
+++ Relational Databases/Assignment1/Client/Client.h
@@ -0,0 +1,69 @@
+/*
+	File: Client.h
+	Name: Matthew Warren, Steven johnston
+	Assignment: Client Server I/O Database Assignment #1
+	Date: 9/25/2015
+	Description: includes, defines, enums, structs, and prototypes required for Client
+*/
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <time.h>
+#include <regex>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#define NAME_LENGTH 32
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+#define MAX_RECORD_COUNT 40000
+#define DOB_ALLOCATE_SIZE 11
+#define MAX_AGE 115
+#define NUM_MONTHS 12
+#define NUM_DAYS 28
+#define MIN_YEAR 1900
+
+enum StatmentType
+{
+	zero,
+	insert,
+	update,
+	find
+}typedef StatmentType;
+struct MemberRecord
+{
+	int memberId;
+	char firstName[NAME_LENGTH];
+	char lastName[NAME_LENGTH];
+	char dOB[DOB_ALLOCATE_SIZE];
+}typedef MemberRecord;
+
+struct ServerCall
+{
+	int callType;
+	MemberRecord member;
+} typedef ServerCall;
+
+struct ClientCall
+{
+	int error;
+	char message[64];
+	MemberRecord member;
+} typedef ClientCall;
+
+bool insertMany(SOCKET ConnectSocket, int quantity);
+int getNum(int min, int max);
+void randomServerCall(ServerCall *message);
+bool findMember(SOCKET ConnectSocket, int memberId);
+bool updateMember(SOCKET ConnectSocket, MemberRecord member);
