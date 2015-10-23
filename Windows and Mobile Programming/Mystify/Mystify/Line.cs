248c5c03c9bc91d7f88c3661ee5ee5e60edf892a
unknown <sdjohnston@2A213-B04.conestogac.on.ca>
2015-10-23 14:36:52 -0400

    Added Comments, Removed color get/set methods

--- Windows and Mobile Programming/Mystify/Mystify/Line.cs
+++ Windows and Mobile Programming/Mystify/Mystify/Line.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Name: Steven Johnston
+ * File: line.cs
+ * Assignment: Mystifiy #03
+ * Date: 10/23/2015
+ * Description: Holds draw function and field for a line
+ */
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -7,69 +14,45 @@ using System.Threading.Tasks;
 
 namespace Mystify
 {
-    /// <summary>
-    /// 
-    /// </summary>
     class Line : Shape
     {
         /// <summary>
-        /// The p1
+        /// first point for the line
         /// </summary>
-        PointF p1;
-        /// <summary>
-        /// Gets or sets the p1.
-        /// </summary>
-        /// <value>
-        /// The p1.
-        /// </value>
-        public PointF P1
-        {
-            get { return p1; }
-            set { p1 = value; }
-        }
-        /// <summary>
-        /// The p2
-        /// </summary>
-        PointF p2;
+        PointF point1;
 
         /// <summary>
-        /// Gets or sets the p2.
+        /// Gets or sets the point1.
         /// </summary>
         /// <value>
-        /// The p2.
+        /// The point1.
         /// </value>
-        public PointF P2
+        public PointF Point1
         {
-            get { return p2; }
-            set { p2 = value; }
+            get { return point1; }
+            set { point1 = value; }
+        }
+        
+        /// <summary>
+        /// second point for the line
+        /// </summary>
+        PointF point2;
+
+        /// <summary>
+        /// Gets or sets the point2.
+        /// </summary>
+        /// <value>
+        /// The point2.
+        /// </value>
+        public PointF Point2
+        {
+            get { return point2; }
+            set { point2 = value; }
         }
         /// <summary>
         /// The random
         /// </summary>
         Random rnd = new Random();
-        /// <summary>
-        /// The c
-        /// </summary>
-        Color c = Color.Black;
-        /// <summary>
-        /// Gets or sets the c.
-        /// </summary>
-        /// <value>
-        /// The c.
-        /// </value>
-        public Color C
-        {
-            get
-            {
-                return c;
-            }
-
-            set
-            {
-                c = value;
-            }
-        }
-
         /// <summary>
         /// The trail
         /// </summary>
@@ -77,19 +60,12 @@ namespace Mystify
         /// <summary>
         /// Initializes a new instance of the <see cref="Line"/> class.
         /// </summary>
-        public Line()
-        { 
-            
-        }
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Line"/> class.
-        /// </summary>
         /// <param name="p1">The p1.</param>
         /// <param name="p2">The p2.</param>
         public Line(PointF p1, PointF p2)
         {
-            this.p1 = p1;
-            this.p2 = p2;
+            this.point1 = p1;
+            this.point2 = p2;
         }
         /// <summary>
         /// Initializes a new instance of the <see cref="Line"/> class.
@@ -99,17 +75,17 @@ namespace Mystify
         /// <param name="newC">The new c.</param>
         public Line(PointF p1, PointF p2,Color newC)
         {
-            this.p1 = p1;
-            this.p2 = p2;
-            c = newC;
+            this.point1 = p1;
+            this.point2 = p2;
+            C = newC;
         }
         /// <summary>
-        /// Draws the specified e.
+        /// Draws this line on e Graphics
         /// </summary>
         /// <param name="e">The e.</param>
         public override void draw(Graphics e)
         {
-            e.DrawLine(new Pen(C),p1,p2);
+            e.DrawLine(new Pen(C),point1,point2);
         }
     }
 }
