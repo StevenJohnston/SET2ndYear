cc31da27befe85ae8566f44b8bf592f64038d32b
unknown <sdjohnston@2A213-B04.conestogac.on.ca>
2015-10-23 13:05:42 -0400

    Added comments using Ghost Doc

--- Windows and Mobile Programming/Mystify/Mystify/frmMystify.cs
+++ Windows and Mobile Programming/Mystify/Mystify/frmMystify.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Name: Steven Johnston
+ * File: frmMystify.cs
+ * Assignment: Mystifiy #03
+ * Date: 10/23/2015
+ * Description: Form for Mystify.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,15 +21,49 @@ namespace Mystify
 {
     public partial class frmMystify : Form
     {
-        private readonly System.Threading.Mutex _m = new System.Threading.Mutex();
+        /// <summary>
+        /// The lock myst shapes
+        /// </summary>
+        private readonly System.Threading.Mutex lockMystShapes = new System.Threading.Mutex();
+        /// <summary>
+        /// The lock timers
+        /// </summary>
+        private readonly System.Threading.Mutex lockTimers = new System.Threading.Mutex();
+        /// <summary>
+        /// The myst shapes
+        /// </summary>
         List<Shape> mystShapes = new List<Shape>();
+        /// <summary>
+        /// The timers
+        /// </summary>
         List<MyTimer> timers = new List<MyTimer>();
+        /// <summary>
+        /// The img
+        /// </summary>
         Bitmap img;
+        /// <summary>
+        /// The g
+        /// </summary>
         Graphics g;
+        /// <summary>
+        /// The draw timer
+        /// </summary>
         MyTimer drawTimer;
+        /// <summary>
+        /// The paused
+        /// </summary>
         bool paused = false;
+        /// <summary>
+        /// The pane width percent
+        /// </summary>
         float paneWidthPercent;
+        /// <summary>
+        /// The pane height percent
+        /// </summary>
         float paneHeightPercent;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="frmMystify"/> class.
+        /// </summary>
         public frmMystify()
         {
             InitializeComponent();
@@ -34,35 +76,43 @@ namespace Mystify
             paneWidthPercent = (float)pnlPane.Width / this.Width;
             paneHeightPercent = (float)pnlPane.Height / this.Height;
         }
+        /// <summary>
+        /// Draws the specified e.
+        /// </summary>
+        /// <param name="e">The e.</param>
         public void draw(object e)
         {
             pnlPane.Invalidate();
         }
 
-        private void frmMystify_Load(object sender, EventArgs e)
-        {
-
-        }
-        
-
+        /// <summary>
+        /// Handles the down event of the pane_mouse control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
         private void pane_mouse_down(object sender, MouseEventArgs e)
         {
             if (!paused)
             {
                 MystLine newMyst = new MystLine(pnlPane, e.Location);
-                MyTimer newTimer = new MyTimer(1000 / 144, newMyst.update, this);
+                MyTimer newTimer = new MyTimer(1000 / trcSpeed.Value, newMyst.update, this);
                 timers.Add(newTimer);
-                lock (_m)
+                lock (lockMystShapes)
                 {
                     mystShapes.Add(newMyst);
                 }
             }
         }
-        
 
+
+        /// <summary>
+        /// Handles the Paint event of the pnlPane control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="PaintEventArgs"/> instance containing the event data.</param>
         private void pnlPane_Paint(object sender, PaintEventArgs e)
         {
-            lock (_m)
+            lock (lockMystShapes)
             {
                 g.Clear(Color.Black);
                 foreach (var myst in mystShapes)
@@ -74,42 +124,78 @@ namespace Mystify
             pnlPane.BackgroundImage = img;
         }
 
+        /// <summary>
+        /// Handles the Click event of the btnPauseResume control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void btnPauseResume_Click(object sender, EventArgs e)
         {
             paused = !paused;
             if (paused)
             {
                 btnPauseResume.Text = "Resume";
-                foreach (var time in timers)
+                lock (lockTimers)
                 {
-                    time.pause();
+                    foreach (var time in timers)
+                    {
+                        time.pause();
+                    }
                 }
             }
             else
             {
                 btnPauseResume.Text = "Pause";
-                foreach (var time in timers)
+                lock (lockTimers)
                 {
-                    time.resume();
+                    foreach (var time in timers)
+                    {
+                        time.resume();
+                    }
                 }
             }
         }
-
-        private void btnShutDown_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Kills the line timers.
+        /// </summary>
+        private void killLineTimers()
         {
-            foreach (var timer in timers)
+            lock (lockTimers)
             {
-                timer.stop();
+                foreach (var timer in timers)
+                {
+                    timer.stop();
+                }
             }
             timers.Clear();
             mystShapes.Clear();
+        
+        }
+        /// <summary>
+        /// Handles the Click event of the btnShutDown control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnShutDown_Click(object sender, EventArgs e)
+        {
+            killLineTimers();
         }
 
+        /// <summary>
+        /// Handles the Click event of the btnDrawTriangle control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void btnDrawTriangle_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Handles the Scroll event of the trcTrail control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void trcTrail_Scroll(object sender, EventArgs e)
         {
             foreach (Shape mystShape in mystShapes)
@@ -118,11 +204,27 @@ namespace Mystify
             }
         }
 
+        /// <summary>
+        /// Handles the Scroll event of the trcSpeed control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void trcSpeed_Scroll(object sender, EventArgs e)
         {
-            Usefull.speed = 1000/trcSpeed.Value;
+            lock (lockTimers)
+            {
+                foreach (var timer in timers)
+                {
+                    timer.DelayAmount = 1000 / trcSpeed.Value;
+                }
+            }
         }
 
+        /// <summary>
+        /// Handles the Resize event of the frmMystify control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void frmMystify_Resize(object sender, EventArgs e)
         {
             pnlPane.Width = (int)(this.Width - pnlTools.Width);
@@ -132,6 +234,17 @@ namespace Mystify
                 img = new Bitmap(pnlPane.Width, pnlPane.Height);
                 g = Graphics.FromImage(img);
             }
+        }
+
+        /// <summary>
+        /// Handles the FormClosing event of the frmMystify control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="FormClosingEventArgs"/> instance containing the event data.</param>
+        private void frmMystify_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            killLineTimers();
+            drawTimer.stop();
         }
     }
 }
