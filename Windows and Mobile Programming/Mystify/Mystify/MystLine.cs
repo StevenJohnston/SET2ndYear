08b8e6830876c271c16967a40924118e7fba4bea
Steven Johnston <wheatthins@hotmail.ca>
2015-10-23 14:56:03 -0400

    Changed color variable name from c to color

--- Windows and Mobile Programming/Mystify/Mystify/MystLine.cs
+++ Windows and Mobile Programming/Mystify/Mystify/MystLine.cs
@@ -60,7 +60,7 @@ namespace Mystify
             pointVelocity2.direction.X *= Usefull.plusMinusOne();
             pointVelocity2.direction.Y *= Usefull.plusMinusOne();
             //Create random colour MIGHT BE BLACK 
-            C = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            Color = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
         }
 
         /// <summary>
@@ -79,9 +79,9 @@ namespace Mystify
                 //Updares the colour of each line. Making them slightly less visable be decresing the Alpha value
                 foreach (var line in lines)
                 {
-                    if (line.C.A - (255 / Usefull.trailLength) >= 0)
+                    if (line.Color.A - (255 / Usefull.trailLength) >= 0)
                     {
-                        line.C = Color.FromArgb(line.C.A - (255 / Usefull.trailLength), line.C);
+                        line.Color = Color.FromArgb(line.Color.A - (255 / Usefull.trailLength), line.Color);
                     }
                 }
                 //Move point one and keep it in the control(Panle)
@@ -91,7 +91,7 @@ namespace Mystify
                 pointVelocity2.move();
                 pointVelocity2.direction = Usefull.keepPointInControl(pointVelocity2.position, pointVelocity2.direction, Usefull.drawPanel);
                 //Add new line(The front line in the Trail) to the list
-                lines.Add(new Line(pointVelocity1.position,pointVelocity2.position, C));
+                lines.Add(new Line(pointVelocity1.position, pointVelocity2.position, Color));
             }
         }
         /// <summary>
