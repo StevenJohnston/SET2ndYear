6e7bd3577ec6e20512b78f514b51d00ec9704079
unknown <sdjohnston@2A213-B04.conestogac.on.ca>
2015-10-23 13:34:07 -0400

    Added comments using Ghost Doc, And updated comments to be more specific

--- Windows and Mobile Programming/Mystify/Mystify/MystLine.cs
+++ Windows and Mobile Programming/Mystify/Mystify/MystLine.cs
@@ -8,39 +8,67 @@ using System.Windows.Forms;
 
 namespace Mystify
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class MystLine : Shape
     {
-        //Line frontLine = new Line();
+        /// <summary>
+        /// The random
+        /// </summary>
         Random rnd = new Random();
-        PointVelocity P1;
-        PointVelocity P2;
+        /// <summary>
+        /// The point and Velocity of first point
+        /// </summary>
+        PointVelocity pointVelocity1;
+        /// <summary>
+        /// The point and Velocity of first point
+        /// </summary>
+        PointVelocity pointVelocity2;
+        /// <summary>
+        /// The lines
+        /// </summary>
         public List<Line> lines = new List<Line>();
-        SETPaint.Pane drawForm;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MystLine" /> class.
+        /// </summary>
         public MystLine()
         {
         
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MystLine" /> class.
+        /// </summary>
+        /// <param name="pane">The pane.</param>
+        /// <param name="start">The start.</param>
         public MystLine(SETPaint.Pane pane, Point start)
         {
-            P1 = new PointVelocity(start, Usefull.randomVelocity(new PointF()));
-            P2 = new PointVelocity(start, Usefull.randomVelocity(new PointF()));
-            drawForm = pane;
+            //Set poistion and speed for point one
+            pointVelocity1 = new PointVelocity(start, Usefull.randomVelocity(new PointF()));
+            pointVelocity1.direction.X *= Usefull.plusMinusOne();
+            pointVelocity1.direction.Y *= Usefull.plusMinusOne();
+            //Set poistion and speed for point two
+            pointVelocity2 = new PointVelocity(start, Usefull.randomVelocity(new PointF()));
+            pointVelocity2.direction.X *= Usefull.plusMinusOne();
+            pointVelocity2.direction.Y *= Usefull.plusMinusOne();
+            //Create random colour MIGHT BE BLACK 
             c = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
         }
-        public MystLine(PointVelocity pV1, PointVelocity pV2)
-        {
-            P1 = pV1;
-            P2 = pV2;
-        }
 
+        /// <summary>
+        /// Updates line postion, line trail, and line colour.
+        /// </summary>
+        /// <param name="e">The e.</param>
         public void update(object e)
         {
-            lock(_m)
+            lock(linesLock)
             {
+                //removes all lines after the maximum number of trail lines
                 for (; lines.Count > Usefull.trailLength;)
                 {
                     lines.RemoveAt(0);
                 }
+                //Updares the colour of each line. Making them slightly less visable be decresing the Alpha value
                 foreach (var line in lines)
                 {
                     if (line.C.A - (255 / Usefull.trailLength) >= 0)
@@ -48,16 +76,23 @@ namespace Mystify
                         line.C = Color.FromArgb(line.C.A - (255 / Usefull.trailLength), line.C);
                     }
                 }
-                P1.move();
-                P1.direction = Usefull.keepPointInControl(P1.position,P1.direction,Usefull.drawPanel);
-                P2.move();
-                P2.direction = Usefull.keepPointInControl(P2.position, P2.direction, Usefull.drawPanel);
-                lines.Add(new Line(P1.position,P2.position, c));
+                //Move point one and keep it in the control(Panle)
+                pointVelocity1.move();
+                pointVelocity1.direction = Usefull.keepPointInControl(pointVelocity1.position, pointVelocity1.direction, Usefull.drawPanel);
+                //Move point two and keep it in the control(Panle)
+                pointVelocity2.move();
+                pointVelocity2.direction = Usefull.keepPointInControl(pointVelocity2.position, pointVelocity2.direction, Usefull.drawPanel);
+                //Add new line(The front line in the Trail) to the list
+                lines.Add(new Line(pointVelocity1.position,pointVelocity2.position, c));
             }
         }
+        /// <summary>
+        /// Calls the draw function of each line in the trail.
+        /// </summary>
+        /// <param name="e">The e.</param>
         public override void draw(Graphics e)
         {
-            lock(_m)
+            lock(linesLock)
             {
                 foreach (var line in lines)
                 {
@@ -65,6 +100,9 @@ namespace Mystify
                 }
             }
         }
-        private readonly System.Threading.Mutex _m = new System.Threading.Mutex();
+        /// <summary>
+        /// The linesLock used to lock the lines list.
+        /// </summary>
+        private readonly System.Threading.Mutex linesLock = new System.Threading.Mutex();
     }
 }
