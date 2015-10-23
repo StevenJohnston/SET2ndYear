2024ec1edbe2dfd79b761f71b3e5e29ad445ced7
Steven Johnston <wheatthins@hotmail.ca>
2015-10-23 15:03:45 -0400

    Added comments including header comment

--- Windows and Mobile Programming/Mystify/Mystify/Usefull.cs
+++ Windows and Mobile Programming/Mystify/Mystify/Usefull.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+    Name: Steven Johnston, Matthew Warren
+    File: Usefull.cs
+    Assignment: SET Paint (#2)
+    Date: 10/8/2015
+    Description: static class to hold Usefull fields and mehtods used throught the project
+*/
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,12 +15,34 @@ using System.Windows.Forms;
 
 namespace Mystify
 {
+    /// <summary>
+    /// 
+    /// </summary>
     static class Usefull
     {
+        /// <summary>
+        /// The trail length
+        /// </summary>
         static public int trailLength = 10;
+        /// <summary>
+        /// The speed the update calls are called
+        /// </summary>
         static public int speed = 1000;
+        /// <summary>
+        /// The control that is drawn to. this allows for width and height to be grabed when resize happens
+        /// </summary>
         static public Control drawPanel;
+        /// <summary>
+        /// The random
+        /// </summary>
         static Random rnd = new Random();
+        /// <summary>
+        /// Keeps the point the in control.
+        /// </summary>
+        /// <param name="p">The p.</param>
+        /// <param name="v">The v.</param>
+        /// <param name="control">The control.</param>
+        /// <returns></returns>
         public static PointF keepPointInControl(PointF p,PointF v, Control control)
         {
             if (p.X < 0)
@@ -41,12 +70,21 @@ namespace Mystify
             }
             return v;
         }
+        /// <summary>
+        /// Randomizes the velocity of a point.
+        /// </summary>
+        /// <param name="p">The p.</param>
+        /// <returns></returns>
         public static PointF randomVelocity(PointF p)
         {
             p.X = rnd.Next(2, 10);
             p.Y = rnd.Next(2, 10);
             return p;
         }
+        /// <summary>
+        /// Pluses the minus one.
+        /// </summary>
+        /// <returns></returns>
         public static int plusMinusOne()
         {
             return rnd.Next(0, 2) == 0 ? -1 : 1;
