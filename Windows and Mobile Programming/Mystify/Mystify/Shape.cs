acd1e7eb2bff79a78ce437b62d6fe7c9af7f9300
Steven Johnston <wheatthins@hotmail.ca>
2015-10-23 14:56:27 -0400

    Changed Color variable name from c to color

--- Windows and Mobile Programming/Mystify/Mystify/Shape.cs
+++ Windows and Mobile Programming/Mystify/Mystify/Shape.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+    Name: Steven Johnston, Matthew Warren
+    File: Shape.cs
+    Assignment: SET Paint (#2)
+    Date: 10/8/2015
+    Description: Shape object that will most often be inherited from
+*/
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -7,11 +14,34 @@ using System.Threading.Tasks;
 
 namespace Mystify
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Shape
     {
-        protected Color c;
+        /// <summary>
+        /// The color of the shape
+        /// </summary>
+        private Color color;
+
+        public Color Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        
+        /// <summary>
+        /// The shape trail
+        /// </summary>
         private int shapeTrail = 10;
 
+        /// <summary>
+        /// Gets or sets the shape trail.
+        /// </summary>
+        /// <value>
+        /// The shape trail.
+        /// </value>
         public int ShapeTrail
         {
             get
@@ -25,9 +55,16 @@ namespace Mystify
             }
         }
 
+        /// <summary>
+        /// Draws the specified e.
+        /// </summary>
+        /// <param name="e">The e.</param>
         public virtual void draw(Graphics e)
         {
         }
+        /// <summary>
+        /// Updates this instance.
+        /// </summary>
         public virtual void update()
         {
         }
