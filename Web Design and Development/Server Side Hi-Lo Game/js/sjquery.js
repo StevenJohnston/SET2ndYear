26960798e62ed2e9ecd4933d318bef207172166d
unknown <sdjohnston@2A213-B03.conestogac.on.ca>
2015-10-26 11:06:30 -0400

    Updated to newest Version of SJQuery

--- Web Design and Development/Server Side Hi-Lo Game/js/sjquery.js
+++ Web Design and Development/Server Side Hi-Lo Game/js/sjquery.js
@@ -3,15 +3,14 @@
 	Make shift JQUERY ($SJ)
 	In seperate script tag so i can make it into a library 
 */
-var elementCallList [];
+var elementCallList = [];
 function startMethod(elementIn)
 {
 
 }
 function nextMethod(elementIn)
 {
-	elementCallList
-
+	elementCallList;
 }
 
 var doEvent = function(elemnt,stackLimiter)
@@ -29,20 +28,14 @@ var doEvent = function(elemnt,stackLimiter)
 //	stackLimiter: If set will not create a second version of its self to save the stack from EXPLODING
 //Return:
 //	An array with a list of all the methods
-var $SJ = function(elementIn,stackLimiter) {
+var $SJ = function(elementIn) {
 	var thisElement ; //Second version of its self allows for easier method calls (on same element)
 	var elements = []; //Helps return an array rather then a nodeList
-	if(stackLimiter == undefined){
-		thisElement = $SJ(elementIn,"defined");
-	}
 
 	//element must be of type string
 	if(typeof elementIn == "string"){
 		//How to select element (by id,name,tag,classname)
 		var newElements;
-		if(stackLimiter == undefined){
-			var thisElement = $SJ(elementIn,"defined");
-		}
 		switch(elementIn.charAt(0)){
 			case '#': //by id
 				elements.push(document.getElementById(elementIn.substring(1)));
@@ -76,7 +69,8 @@ var $SJ = function(elementIn,stackLimiter) {
 				break;
 		}
 	}
-
+	GiveIDifNotExist(elements);
+	allocateElementsInList(elements);
 	//Array of methods
     return {
     	//Name: innerHTML
@@ -90,11 +84,17 @@ var $SJ = function(elementIn,stackLimiter) {
         	{
 	        	for(var element in elements)
 	        	{
-	        		elementCallList[element].push = {"function" : function(element,val){element.innerHTML = val},
-	        											"val" : val};
-	            	//elements[element].innerHTML = val;
+	        		
+	        		elementCallList[elements[element].id].callList.push({"func" : function(element,val){element.innerHTML = val},
+	        														"val" : val,"delay" : 0});
+
+					if(elementCallList[elements[element].id].running == false)
+					{
+						elementCallList[elements[element].id].running = true;
+						dequeue(elements[element].id);
+					}
 	        	}
-	        	return thisElement;
+	        	return $SJ(elementIn,"defined");
 	        }else
 	        {
 	        	var innerHTMLs = [];
@@ -109,6 +109,22 @@ var $SJ = function(elementIn,stackLimiter) {
 	        	else
 	        		return innerHTMLs;
 	        }
+        },
+        delay: function(val)
+        {
+        	for(var element in elements)
+        	{
+        		
+        		elementCallList[elements[element].id].callList.push({"func" : function(element,val){},
+        														"val" : val,"delay" : val});
+
+				if(elementCallList[elements[element].id].running == false)
+				{
+					elementCallList[elements[element].id].running = true;
+					dequeue(elements[element].id);
+				}
+        	}
+        	return $SJ(elementIn,"defined");
         },/*
     	//Name: style
 		//Decsription: set specified style attribute to specified value
@@ -377,3 +393,52 @@ var $SJ = function(elementIn,stackLimiter) {
         },*/
     }
 };
+
+//Get next free id and return it
+var nextID =0;
+function getNextID()
+{
+	for(;document.getElementById(nextID) != undefined; nextID++){}
+	return nextID++;
+}
+//Gives each element a unique ID if it doesnt have one 
+function GiveIDifNotExist(elementArray)
+{
+	for(var element in elementArray)
+	{
+		if(elementArray[element].id == "")
+		{
+			elementArray[element].id = getNextID();
+		}
+	}
+}
+
+function allocateElementsInList(elementArray)
+{
+	for(var element in elementArray)
+	{
+		if(elementCallList[elementArray[element].id] == undefined)
+		{
+			elementCallList[elementArray[element].id] = {};
+			elementCallList[elementArray[element].id].elementObject = elementArray[element];
+			elementCallList[elementArray[element].id].running = false;
+			elementCallList[elementArray[element].id].callList = [];
+		}
+	}
+}
+
+function dequeue(elementIndex) 
+{
+	var thisEle = elementCallList[elementIndex];
+	if(thisEle.callList.length != 0)
+	{
+		setTimeout(function(){
+			thisEle.callList[0].func(thisEle.elementObject,thisEle.callList[0].val);
+			thisEle.callList.shift();
+			dequeue(elementIndex);
+		},thisEle.callList[0].delay);
+	}
+	thisEle.running = false;
+	//var thisEle = elementCallList[elementIndex];
+	//thisEle.callList[0].func(thisEle.elementObject,thisEle.callList[0].val);
+}
